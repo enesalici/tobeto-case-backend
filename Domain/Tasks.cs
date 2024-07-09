0274cdf7c35c46d2469d5a378fa166b1789dd633
Enes Alıcı <enesaalici@gmail.com>
2024-07-09 20:34:24 +0300

    Add project files.

---
+++ Domain/Tasks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; } 
+        public TaskStatus Status { get; set; } 
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
+
+public enum TaskStatus
+{
+    New,
+    InProgress,
+    Completed
+}
